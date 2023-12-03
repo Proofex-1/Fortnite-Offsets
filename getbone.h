5dd51ab5cf7748a80931a92c20a364d6b0dba2c7
Proofex <141692791+Proofex-1@users.noreply.github.com>
2023-12-03 17:55:29 +0100

    Update getbone.h

--- getbone.h
+++ getbone.h
@@ -1,7 +1,7 @@
-Vector3 GetBoneWithRotation(uintptr_t mesh, int bone_id)
+eVector3 GetBoneWithRotation(uintptr_t mesh, int bone_id)
 {
-	uintptr_t bone_array = read<uintptr_t>(mesh + 0x600);
-	if (bone_array == NULL) bone_array = read<uintptr_t>(mesh + 0x600 + 0x10);
+	uintptr_t bone_array = read<uintptr_t>(mesh + 0x620);
+	if (bone_array == NULL) bone_array = read<uintptr_t>(mesh + 0x620 + 0x10);
 	FTransform bone = read<FTransform>(bone_array + (bone_id * 0x60));
 	FTransform component_to_world = read<FTransform>(mesh + 0x240);
 	D3DMATRIX matrix = MatrixMultiplication(bone.ToMatrixWithScale(), component_to_world.ToMatrixWithScale());
